249708949a2d929ec53a63220f8feef8aac2a794
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-06-06 11:55:51 -0500

    feat: added function to display a message using a promise

--- index.js
+++ index.js
@@ -54,3 +54,18 @@ function calculateSides(side) {
 }
 
 calculateSides(10);
+
+// Promises - Promises allow us to identify when a function was not executed correctly, they are error handlers.
+// use case: Verify that the information is obtained correctly.
+
+var myPromise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve('La informacion se ha obtenido correctamete');
+    }, 3000);
+});
+
+myPromise.then(function(value) {
+    console.log(value);
+});
+
+console.log(myPromise);
