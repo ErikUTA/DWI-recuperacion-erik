5e85cd7cbd45574f27af734b6fcb4b7c4cdc53c6
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-06-06 11:40:19 -0500

    feat: added the function to find the first matching element

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ const newSalary = salary.map(e => {
 const persons = [20, 17, 19, 15];
 const accepted = persons.filter(element => element > 18);
 console.log(accepted);
+
+// find - The find function returns the first element that meets the condition.
+// use case: show first number greater than 18.
+
+const numbers = [10, 18, 19, 20];
+const found = numbers.find(element => element > 18);
+console.log(found);
