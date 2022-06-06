f48dc1c4558b959c3273b65cccf23d1306795d04
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-06-06 11:53:53 -0500

    feat: added a function to sum the sides of a triangle using hoisting

--- index.js
+++ index.js
@@ -44,3 +44,13 @@ console.log(names[index]);
 const string = "Juan, Fernanda, Erik, Eduardo";
 const result = string.replace('Erik', 'Torres');
 console.log(result);
+
+// Hoisting - Thanks to hoisting we can use function variables before declaring them in the code.
+// use case: Calculate the sum of the sides of a triangle.
+
+const sides = [10, 10];
+function calculateSides(side) {
+    console.log("sum of the sides: " + (sides[0] + sides[1] + side));
+}
+
+calculateSides(10);
