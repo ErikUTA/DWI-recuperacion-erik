f36e7723f3ba87b4cc6671c21f8eb1ada0dfc446
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-06-06 11:46:19 -0500

    feat: added the function to get the matching element using findIndex

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ const sumAllSales = sales.reduce(
     (prev, current) => prev + current, initialValue
 );
 console.log(sumAllSales);
+
+// findIndex - The "FindIndex" function returns the index of the element that meets the given condition.
+// use case: Obtain the name of the customer with name "Erik".
+
+const names = ["Juan", "Fernanda", "Erik", "Eduardo"];
+const index = names.findIndex(i => i === "Erik");
+console.log(index);
+console.log(names[index]);
