1da3850393a1b026f68de1ba933dd8ccbf8d568a
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-06-06 11:36:40 -0500

    feat: added filter function to create an array with the elements that meet the condition

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const salary = [12000, 7000, 15000, 20000, 7000, 15000];
 const newSalary = salary.map(e => {
     console.log(e + 1000);
 });
+
+// filter - The filter function allows us to create a new array with the elements that meet the given condition.
+// use case: show people over 18 years old.
+
+const persons = [20, 17, 19, 15];
+const accepted = persons.filter(element => element > 18);
+console.log(accepted);
