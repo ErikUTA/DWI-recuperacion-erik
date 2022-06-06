fb57a3a01e2307d040040b842dc2fd90b72cc6db
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-06-06 11:43:17 -0500

    feat: added a new function to show the sum of the numbers in array

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ console.log(accepted);
 const numbers = [10, 18, 19, 20];
 const found = numbers.find(element => element > 18);
 console.log(found);
+
+// reduce - The function "reduce" allows us to add the values of an array and then return them.
+// use case: show last week's sales total.
+
+const sales = [1000, 5000, 8000, 2000];
+const initialValue = 0;
+const sumAllSales = sales.reduce(
+    (prev, current) => prev + current, initialValue
+);
+console.log(sumAllSales);
